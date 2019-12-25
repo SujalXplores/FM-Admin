ad8b3d49b2cee5a385fd139b35b69edff5895872
DhruvModi09 <DhruvModi09>
2019-12-25 14:01:15 +0530

    angular signup

--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -3,12 +3,12 @@ export class users {
   public constructor(
     public u_email_id: string,
     public u_password: string,
-    public u_name: string,
-    public u_gender: string,
-    public u_mobileno: number,
-    public u_dob: Date,
-    public u_address: string,
-    public u_image: string,
+    public u_name?: string,
+    public u_gende?: string,
+    public u_mobileno?: number,
+    public u_dob ?: Date,
+    public u_address ?: string,
+    public u_image ?: string,
   ) {
 
   }
