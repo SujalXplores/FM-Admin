6339294a7bb4af1b83852162d37c05c25c64a9a8
DhruvModi09 <DhruvModi09>
2020-01-06 16:09:51 +0530

    order table changed

--- src/app/order/order.ts
+++ src/app/order/order.ts
@@ -1,10 +1,11 @@
 export class order {
 
   public constructor(
-    public order_id: string,
+    public order_id: number,
     public order_amount: number,
     public order_date: Date,
     public fk_u_email_id: string,
     public payment_type: string,
+    public order_status: string,
   ) {}
 }
