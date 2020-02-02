509d81b27a505d0b4b1cb4e0d48b218854628309
birenrana <ranabiren06@gmail.com>
2020-02-02 20:52:58 +0530

    bootstarp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { UsersComponent } from './users/users.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatExpansionModule,MatButtonToggleModule,MatSnackBarModule,MatTooltipModule,MatMenuModule,MatPaginatorModule, MatNativeDateModule, MatProgressBarModule, MatSortModule, MatTableModule, MatSelectModule, MatCardModule, MatRadioModule, MatInputModule, MatCheckboxModule, MatDialogModule, MatFormFieldModule} from '@angular/material';
 import { routingArr } from './app.routing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -106,7 +105,7 @@ import { ChangePasswordComponent } from './change-password/change-password.compo
     LayoutModule,
     FormsModule,
     ReactiveFormsModule,
-    NgbModule,
+
     //angular-material
     MatToolbarModule,
     MatMenuModule,
