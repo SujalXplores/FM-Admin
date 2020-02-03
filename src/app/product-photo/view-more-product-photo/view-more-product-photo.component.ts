b47879b0824c167b2f6ad509ad7bdb176c25ef55
sujalshah3234 <shahc9437@gmail.com>
2020-02-03 22:14:50 +0530

    many major bug fixes. dialog bugs removed. buggy buttons fixed. images updated.

--- src/app/product-photo/view-more-product-photo/view-more-product-photo.component.ts
+++ src/app/product-photo/view-more-product-photo/view-more-product-photo.component.ts
@@ -9,19 +9,21 @@ import { product_photo } from '../product-photo';
 })
 export class ViewMoreProductPhotoComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<ViewMoreProductPhotoComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: product_photo) { }
+constructor(public dialogRef: MatDialogRef<ViewMoreProductPhotoComponent>,
+@Inject(MAT_DIALOG_DATA) public data: product_photo) { }
 
-    product_photo_id: number;
-    fk_product_id: number;
+product_photo_id: number;
+fk_product_id: number;
+pro_photo: string;
 
-  ngOnInit() {
-    this.product_photo_id = this.data.pro_photo_id;
-    this.fk_product_id = this.data.fk_pro_id;
-  }
+ngOnInit() {
+  this.product_photo_id = this.data.pro_photo_id;
+  this.fk_product_id = this.data.fk_pro_id;
+  this.pro_photo = this.data.pro_photo;
+}
 
-  onCancelClick() {
-    this.dialogRef.close();
-  }
+onCancelClick() {
+  this.dialogRef.close();
+}
 
 }
