ea4c432aa66dd27b9c0d3b7ed5c279444063f6c4
sujalshah3234 <shahc9437@gmail.com>
2020-02-04 20:16:27 +0530

    major improvements in style and functionality. speed improved.

--- src/app/login-display/login-display.component.ts
+++ src/app/login-display/login-display.component.ts
@@ -11,30 +11,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-display.component.css']
 })
 export class LoginDisplayComponent implements OnInit {
-  toasts: any[] = [];
-  loginForm: FormGroup;
+toasts: any[] = [];
+loginForm: FormGroup;
 
-  constructor(private _router:Router, private _logindata: LogindataService) { }
+constructor(private _router:Router, private _logindata: LogindataService) { }
+hide: string = 'false';
+ngOnInit() {
+    this.loginForm = new FormGroup({
+      u_email_id: new FormControl("sujalshah@gmail.com", [Validators.required, Validators.email]),
+      u_password: new FormControl(1234, [Validators.required]),
+    });
+}
 
-  hide: string = 'false';
-  ngOnInit() {
-      this.loginForm = new FormGroup({
-        u_email_id: new FormControl("sujalshah@gmail.com", [Validators.required, Validators.email]),
-        u_password: new FormControl(1234, [Validators.required]),
-      });
-  }
-
-  onLogin() {
-       this._logindata.login(this.loginForm.value).subscribe(
-         (x: users[]) => {
-           if (x.length == 1){
-                localStorage.setItem('u_email_id', this.loginForm.get('u_email_id').value);
-                this._router.navigate(['/nav/']);
-           }
-           else {
-             alert("Invalid Credentials");
-           }
-         }
-       );
+onLogin() {
+    this._logindata.login(this.loginForm.value).subscribe(
+      (x: users[]) => {
+        if (x.length == 1){
+            localStorage.setItem('u_email_id', this.loginForm.get('u_email_id').value);
+            this._router.navigate(['/nav/']);
+        }
+        else {
+          alert("Please Check Your Credentials!");
+        }
       }
-    }
+    );
+  }
+}
