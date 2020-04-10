c28c7e6736656aff1b8028e8238ec5ff11ae0872
sujalshah3234 <shahc9437@gmail.com>
2020-04-10 23:36:14 +0530

    order details mega update

--- src/app/multiple-order-display-component/multiple-order-display-component.component.ts
+++ src/app/multiple-order-display-component/multiple-order-display-component.component.ts
@@ -13,7 +13,11 @@ export class MultipleOrderDisplayComponentComponent implements OnInit {
   order_id: number;
   order_total:number;
   pro_photo: string;
-
+  fk_u_email_id: string;
+  order_date: string;
+  payment_type: string;
+  order_status: string;
+  order_amount: number;
   ordermultiple: any[] = [];
   constructor(public orderService: OrderdataService, public _activated_routes: ActivatedRoute) { }
 
@@ -23,6 +27,11 @@ export class MultipleOrderDisplayComponentComponent implements OnInit {
       (data: any[]) => {
         console.log(data);
         this.ordermultiple = data;
+        this.fk_u_email_id = data[0].fk_u_email_id;
+        this.order_date = data[0].order_date;
+        this.payment_type = data[0].payment_type;
+        this.order_status = data[0].order_status;
+        this.order_amount = data[0].order_amount;
       }
     )
   }
--- src/app/product-photo/editproduct-photo/editproduct-photo.component.ts
+++ src/app/product-photo/editproduct-photo/editproduct-photo.component.ts
@@ -71,10 +71,9 @@ export class EditproductPhotoComponent implements OnInit {
     this._ser.updateProductPhoto(this.pro_photo_id,fd).subscribe(
       (data: product_photo) => {
         console.log(data)
-        alert("Sucessfully edited");
+        this.notificationService.success('Changes has been saved.');
         this._router.navigate(['/nav/product_photo']);
       }
     );
-    this.notificationService.success('Changes has been saved.');
   }
 }
