803d5c3770de154cfcceae94c02067446adfeac7
DhruvModi09 <DhruvModi09>
2020-05-08 16:05:37 +0530

    pie chart data to console

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
-import { Chart } from 'chart.js';
-import { DashboarddataService } from './dashboarddata.service';
+import { Component } from "@angular/core";
+import { Chart } from "chart.js";
+import { DashboarddataService } from "./dashboarddata.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"],
 })
 export class DashboardComponent {
-
   constructor(public _data: DashboarddataService) {}
 
+  public order_data: any[];
   public bill_data: any[];
   public bill_data_display: any[] = [];
   public bill_data_name_display: any[] = [];
+  public order_date: any[] = [];
+  public order_amount: any[] = [];
+  // public paypalAmount: number=0;
+  // public Cash_On_Dlivery_Amount: number=120;
+  // public card_Amount: number=500;
 
   ngOnInit() {
+    this._data.getAllorder().subscribe((data1: any[]) => {
+      this.order_data = data1;
+      for (let i = 0; i < data1.length; i++) {
+        this.order_date.push(this.order_data[i].order_date);
+        this.order_amount.push(this.order_data[i].order_amount);
+      }
+      console.log(this.order_data);
+    });
+
     this._data.getTopOrder().subscribe((data1: any[]) => {
       this.bill_data = data1;
       for (let i = 0; i < data1.length; i++) {
@@ -24,29 +38,63 @@ export class DashboardComponent {
       }
       console.log(this.bill_data_display);
     });
+
+    // this._data.getInvoiceByMode("Paypal").subscribe((data: any) => {
+    //   console.log(data);
+    //   console.log(data[0].total);
+    //   if (data[0].total) {
+    //     this.paypalAmount = data[0].total;
+    //   } else {
+    //     this.paypalAmount = 0;
+    //   }
+    //   console.log(this.paypalAmount);
+    // });
+
+    // this._data.getInvoiceByMode("Cash On Delivery").subscribe((data: any) => {
+    //   console.log(data);
+    //   if (data[0].total) {
+    //     this.Cash_On_Dlivery_Amount = data[0].total;
+    //   } else {
+    //     this.Cash_On_Dlivery_Amount = 0;
+    //   }
+    //   console.log(this.Cash_On_Dlivery_Amount);
+    // });
+
+    // this._data.getInvoiceByMode("Card").subscribe((data: any) => {
+    //   console.log(data);
+    //   if (data[0].total) {
+    //     this.card_Amount = data[0].total;
+    //   } else {
+    //     this.card_Amount = 0;
+    //   }
+    //   console.log(this.card_Amount);
+    // });
   }
-  
+
   public pieData: any[] = [
-    { category: "Cash on Delivery", value: 40 },
-    { category: "Paypal", value: 50 },
-    { category: "Credit/Debit Card", value: 10 },
+    { category: "Cash On Delivery", value: 40 },
+    { category: "Paypal", value: 30 },
+    { category: "Card", value: 30 },
   ];
 
   public series: any[] = [
     {
       name: "Medicines",
-      data: [
-        200,500,750,300,200,100,50,80,86,80,124,321
-      ],
-    },
-    {
-      name: "HealthCare Products",
-      data: [
-        100,200,450,100,40,10,90,100,806,890,324,121
-      ],
+      data: [200, 500, 750, 300, 200, 100, 50, 80, 86, 80, 124, 321],
     },
   ];
   public categories: string[] = [
-    "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
   ];
 }
--- src/app/dashboard/dashboarddata.service.ts
+++ src/app/dashboard/dashboarddata.service.ts
@@ -7,10 +7,21 @@ import { HttpClient } from '@angular/common/http';
 export class DashboarddataService {
 
   url: string = 'http://localhost:3000/dashboardTrendingPro/';
+  order_url: string = 'http://localhost:3000/ordersbyDate/';
+  invoice_url: string = 'http://localhost:3000/invoicemode/'
 
   constructor(public _http: HttpClient) { }
 
   getTopOrder(){
     return this._http.get(this.url);
+  }
+
+  getAllorder(){
+    return this._http.get(this.order_url);
+  }
+
+  getInvoiceByMode(PaymentMODE)
+  {
+    return this._http.get(this.invoice_url+PaymentMODE);
   }
 }
